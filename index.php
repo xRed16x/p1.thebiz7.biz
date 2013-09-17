4edf16b4bca5485e6c6f24470b4264385eeb4518
Edward Sanchez <edwardsanchez@fas.harvard.edu>
2013-09-17 13:59:41 -0400

    Added index and styles

--- index.php
+++ index.php
@@ -3,8 +3,21 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; 
 	charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="styles.css"/>
 	<title>hw example</title>
 </head>
 <body>
-<p>Welcome to P1</p>
+<div id="intro">
+	<h1>Welcome to P1</h1>
+</div>
+<div id="bio">
+	<h3>Biography</h3>
+	<p>My name is Edward Sanchez. I'm originally from southern California, where I got 
+	my associates degree.<br> I came out to Cambridge MA., 
+	in order to continue my education where I plan<br> to get 
+	a Bachelors in Economics, with a minor in Computer Science. 
+	I'm taking this course<br> in order to learn how to build my own website.</p>
+</div>
+<div id="edit">
+	<h4><em>Code Editor Used: Notepad++</em></h4>
 </body>
